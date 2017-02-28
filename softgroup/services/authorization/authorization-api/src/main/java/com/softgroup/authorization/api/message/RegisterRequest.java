45e59073e9365e43374f4bd80b9562c2e0e1074e
polev <maria.polevaja@gmail.com>
2017-02-28 22:50:46 +0200

    main types of message request and response
    (cherry picked from commit 91a9bce)

--- softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/RegisterRequest.java
+++ softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/RegisterRequest.java
@@ -7,7 +7,6 @@ import com.softgroup.common.protocol.RequestData;
  * @since 20.02.17.
  */
 public class RegisterRequest implements RequestData{
-
 	private static final long serialVersionUID = -645554380912935546L;
 
 	private String phoneNumber;
