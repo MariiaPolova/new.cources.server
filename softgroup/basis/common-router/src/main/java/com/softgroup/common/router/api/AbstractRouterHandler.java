5ad481c12c68040d84dac4ec3347f89d495b7d6c
polev <maria.polevaja@gmail.com>
2017-02-28 22:51:55 +0200

    updated abstract classes

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRouterHandler.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRouterHandler.java
@@ -2,17 +2,38 @@ package com.softgroup.common.router.api;
 
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractRouterHandler<T extends Handler> implements RouterHandler {
 
-	@Override
-	public String getRouteKey(Request<?> msg) {
+	private Map<String, T> handlerMap = new HashMap();
+
+	@Autowired
+	private List<T> handlers;
+
+	@PostConstruct
+	public void init(){
+		for(T handler : handlers) {
+			handlerMap.put(handler.getName(), handler);
+		}
+	}
+
+	public String getName() {
 		return null;
 	}
 
-	@Override
 	public Response<?> handle(Request<?> msg) {
-		return null;
+		//String command = msg.getHeader().getCommand();
+		return handlerMap.get(getRouteKey(msg)).handle(msg);
+	}
+
+	public String getRouteKey(Request<?> msg) {
+		return msg.getHeader().getCommand();
 	}
 
 }
