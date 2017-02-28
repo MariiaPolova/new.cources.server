91a9bcee4c3b760e127cf3e8b394ceac217cc423
polev <maria.polevaja@gmail.com>
2017-02-28 22:50:46 +0200

    main types of message request and response

---
+++ softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/SmsConfirmResponse.java
@@ -0,0 +1,18 @@
+package com.softgroup.authorization.api.message;
+
+import com.softgroup.common.protocol.ResponseData;
+
+/**
+ * Created by polev on 28.02.2017.
+ */
+public class SmsConfirmResponse implements ResponseData {
+    private String deviceToken;
+
+    public String getDeviceToken() {
+        return deviceToken;
+    }
+
+    public void setDeviceToken(String deviceToken) {
+        this.deviceToken = deviceToken;
+    }
+}
