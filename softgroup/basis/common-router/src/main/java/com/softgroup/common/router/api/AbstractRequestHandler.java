26d43ab799a6dcfceb93b009ef023eb9d73b2b83
polev <maria.polevaja@gmail.com>
2017-03-01 17:55:02 +0200

    fixed processing of data field

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRequestHandler.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRequestHandler.java
@@ -1,26 +1,34 @@
 package com.softgroup.common.router.api;
 
 
+import com.softgroup.common.datamapper.JacksonDataMapper;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.RequestData;
 import com.softgroup.common.protocol.Response;
 import com.softgroup.common.protocol.ResponseData;
 
+import java.lang.reflect.ParameterizedType;
+import java.util.Map;
+
 public abstract class AbstractRequestHandler<T extends RequestData, R extends ResponseData> implements RequestHandler {
-	//	@Override
-//	public String getName() {
-//		return null;
-//	}
+
+	private JacksonDataMapper dataMapper;
 
 	@Override
 	public Response<R> handle(Request<?> msg) {
 		Request<T> requestObj = new Request<T>();
 		requestObj.setHeader(msg.getHeader());
-		requestObj.setData((T)msg.getData());
 
-		//requestObj is created after Jackson mapper conversion
+
+		Class<T> persistentClass = (Class<T>)((ParameterizedType)getClass().getGenericSuperclass())
+						.getActualTypeArguments()[0];
+		T object = dataMapper.convert((Map<String, Object>)msg.getData(), persistentClass);
+		requestObj.setData(object);
+
 		return doHandle(requestObj);
 	}
+
+	public abstract String getName();
 
 	public abstract Response<R> doHandle(Request<T> requestObj);
 
