a39c7a1928a6ae5f4cc742d6957ce5f64d17ad0a
polev <maria.polevaja@gmail.com>
2017-03-22 01:16:49 +0200

    last fix of bug with router factory

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/factory/RouterHandlerFactory.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/factory/RouterHandlerFactory.java
@@ -2,6 +2,7 @@ package com.softgroup.common.router.api.factory;
 
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.router.api.AbstractRequestHandler;
+import com.softgroup.common.router.api.AbstractRouterHandler;
 import com.softgroup.common.router.api.RouterHandler;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
  * Created by polev on 02.03.2017.
  */
 @Component
-public class RouterHandlerFactory <T extends AbstractRequestHandler> extends HandlerFactory<T> {
+public class RouterHandlerFactory <T extends AbstractRouterHandler> extends HandlerFactory<T> {
     @Override
     public String getCommand(Request<?> msg) {
         return msg.getHeader().getType();
