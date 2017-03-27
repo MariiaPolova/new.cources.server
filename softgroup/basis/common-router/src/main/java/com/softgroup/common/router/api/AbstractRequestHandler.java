0e9490ca5ccb7ba4127fdb95e903a5f2f1ef3e85
polev <maria.polevaja@gmail.com>
2017-03-27 15:17:23 +0300

    mapping added to abstract request handler

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRequestHandler.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRequestHandler.java
@@ -1,27 +1,38 @@
 package com.softgroup.common.router.api;
 
 
+import com.softgroup.common.datamapper.DataMapper;
+import com.softgroup.common.datamapper.JacksonDataMapper;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.RequestData;
 import com.softgroup.common.protocol.Response;
 import com.softgroup.common.protocol.ResponseData;
+import com.softgroup.common.router.api.factory.RequestHandlerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Map;
 
 public abstract class AbstractRequestHandler<T extends RequestData, R extends ResponseData>
 		implements RequestHandler {
-//	@Override
-//	public String getName() {
-//		return null;
-//	}
+
+	@Autowired
+	private DataMapper jacksonDataMapper;
+
+	private final Class<T> persistentClass = (Class<T>)((ParameterizedType)getClass().getGenericSuperclass())
+			.getActualTypeArguments()[0];
 
 	@Override
 	public Response<R> handle(Request<?> msg) {
-		Request<T> requestObj = new Request<T>();
+		Request<T> requestObj = new Request<>();
 		requestObj.setHeader(msg.getHeader());
 
-		//requestObj is created after Jackson mapper conversion
+		T object = jacksonDataMapper.convert((Map<String, Object>)msg.getData(), persistentClass);
+		requestObj.setData(object);
+
 		return doHandle(requestObj);
 	}
 
-	public abstract Response<R> doHandle(Request<?> requestObj);
+	public abstract Response<R> doHandle(Request<T> requestObj);
 
 }
