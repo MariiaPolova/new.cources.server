0397dfe282a008587907ff2bf4d6151c84e16bc7
polev <maria.polevaja@gmail.com>
2017-04-08 12:38:48 +0300

    renamed Handler Factory as implementation

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/factory/HandlerFactory.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/factory/HandlerFactory.java
@@ -2,37 +2,11 @@ package com.softgroup.common.router.api.factory;
 
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.router.api.Handler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
- * Created by polev on 15.03.2017.
+ * Created by polev on 07.04.2017.
  */
+public interface HandlerFactory<T extends Handler> {
 
-public abstract class HandlerFactory<T extends Handler> {
-
-    private Map<String, T> handlerMap = new HashMap<>();
-
-    @Autowired
-    private List<T> handlers;
-
-    @PostConstruct
-    public void init(){
-        for(T handler : handlers) {
-            handlerMap.put(handler.getName(), handler);
-        }
-    }
-
-    public abstract String getCommand(Request<?> msg);
-
-    public T getHandler(Request<?> msg) {
-        String command = getCommand(msg);
-        return handlerMap.get(command);
-    }
-
+    T getHandler(Request<?> msg);
 }
